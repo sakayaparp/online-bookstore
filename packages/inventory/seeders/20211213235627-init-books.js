b03586824ead4ded549e7b51308ee698133a729d
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-14 07:29:33 +0700

    [WD] Applied the Sequelize with database migration for Books

---
+++ packages/inventory/seeders/20211213235627-init-books.js
@@ -0,0 +1,278 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        return queryInterface.bulkInsert('Books', [
+            {
+
+                "bookName": "brown fox ju",
+                "author": "Shayne",
+                "price": 7379.83,
+                "createdAt": "2002-12-23T22:43:47.833Z",
+                "updatedAt": "1985-08-10T22:25:06.979Z"
+            },
+            {
+
+                "bookName": "fox",
+                "author": "Matthew",
+                "price": 1804.69,
+                "createdAt": "2017-11-02T13:17:01.366Z",
+                "updatedAt": "2017-09-12T11:51:51.741Z"
+            },
+            {
+
+                "bookName": "fox jumps over the lazy dogTh",
+                "author": "Joey",
+                "price": 2497.75,
+                "createdAt": "2005-07-08T08:12:13.338Z",
+                "updatedAt": "1997-05-21T03:53:02.241Z"
+            },
+            {
+
+                "bookName": "brown fo",
+                "author": "Petunia",
+                "price": 1068.99,
+                "createdAt": "2008-09-23T03:03:08.104Z",
+                "updatedAt": "1988-01-31T06:22:25.212Z"
+            },
+            {
+
+                "bookName": "fox jumps over the lazy dogT",
+                "author": "Leonora",
+                "price": 6015.78,
+                "createdAt": "1980-08-30T11:19:04.628Z",
+                "updatedAt": "2017-11-29T04:45:42.808Z"
+            },
+            {
+
+                "bookName": "over ",
+                "author": "Kenneth",
+                "price": 7705.85,
+                "createdAt": "1989-03-25T23:08:47.284Z",
+                "updatedAt": "2017-08-01T02:23:03.579Z"
+            },
+            {
+
+                "bookName": "fox jumps over the lazy ",
+                "author": "Yasmin",
+                "price": 4816.1,
+                "createdAt": "2001-01-29T00:12:28.727Z",
+                "updatedAt": "2000-08-29T08:14:10.594Z"
+            },
+            {
+
+                "bookName": "lazy dogThe quick brown",
+                "author": "Lucinda",
+                "price": 226.52,
+                "createdAt": "2018-06-16T22:21:10.534Z",
+                "updatedAt": "2010-02-12T11:45:52.366Z"
+            },
+            {
+
+                "bookName": "ove",
+                "author": "Mehitable",
+                "price": 9516.53,
+                "createdAt": "1985-12-03T15:44:56.524Z",
+                "updatedAt": "2019-09-17T21:48:34.770Z"
+            },
+            {
+
+                "bookName": "o",
+                "author": "Wilfried",
+                "price": 6422.26,
+                "createdAt": "2010-06-18T19:17:01.716Z",
+                "updatedAt": "1994-01-26T01:11:43.314Z"
+            },
+            {
+
+                "bookName": "brown fox",
+                "author": "Erika",
+                "price": 4810.41,
+                "createdAt": "1996-12-31T12:37:36.048Z",
+                "updatedAt": "1986-05-11T16:45:38.113Z"
+            },
+            {
+
+                "bookName": "lazy dogThe quic",
+                "author": "Annabelle",
+                "price": 8961.5,
+                "createdAt": "1986-04-20T03:39:19.426Z",
+                "updatedAt": "2004-07-29T13:38:04.977Z"
+            },
+            {
+
+                "bookName": "over the lazy dogThe quick brown",
+                "author": "Rex",
+                "price": 9253.41,
+                "createdAt": "2010-06-09T19:14:22.869Z",
+                "updatedAt": "1993-08-03T03:03:02.920Z"
+            },
+            {
+
+                "bookName": "dogThe",
+                "author": "Dobie",
+                "price": 2821.83,
+                "createdAt": "2004-12-03T11:23:43.264Z",
+                "updatedAt": "1981-12-27T04:53:32.275Z"
+            },
+            {
+
+                "bookName": "dog",
+                "author": "Mariah",
+                "price": 3173.84,
+                "createdAt": "1997-01-02T14:18:37.610Z",
+                "updatedAt": "1985-09-14T08:19:22.549Z"
+            },
+            {
+
+                "bookName": "dogThe quick brown fox jumps over the lazy",
+                "author": "Harriet",
+                "price": 2137.92,
+                "createdAt": "2016-01-11T00:23:08.831Z",
+                "updatedAt": "1998-06-23T02:40:43.846Z"
+            },
+            {
+
+                "bookName": "dogThe quick brown fox jumps",
+                "author": "Francis",
+                "price": 6324.5,
+                "createdAt": "1985-10-05T17:16:46.539Z",
+                "updatedAt": "2007-07-29T06:24:14.448Z"
+            },
+            {
+
+                "bookName": "over the lazy dogThe quick brown fox j",
+                "author": "Jessica",
+                "price": 1864.99,
+                "createdAt": "2008-04-04T14:34:15.641Z",
+                "updatedAt": "1999-07-10T18:24:04.574Z"
+            },
+            {
+
+                "bookName": "brown fox jumps over the lazy dogThe quick",
+                "author": "Susanne",
+                "price": 9237.38,
+                "createdAt": "1998-02-05T07:57:18.366Z",
+                "updatedAt": "1997-03-13T07:35:10.879Z"
+            },
+            {
+
+                "bookName": "dogThe quick brown fox jumps over th",
+                "author": "Edwina",
+                "price": 5895.79,
+                "createdAt": "2008-04-29T05:12:08.117Z",
+                "updatedAt": "2005-05-22T08:37:19.563Z"
+            },
+            {
+
+                "bookName": "brown fox jumps over the lazy dogThe quick",
+                "author": "Benjamin",
+                "price": 7474.73,
+                "createdAt": "2013-04-05T07:36:57.695Z",
+                "updatedAt": "1980-11-26T14:06:45.375Z"
+            },
+            {
+
+                "bookName": "gThe quick brown fox jumps over the la",
+                "author": "Arabella",
+                "price": 5382.24,
+                "createdAt": "1995-03-03T18:24:58.865Z",
+                "updatedAt": "1986-12-06T04:59:25.623Z"
+            },
+            {
+
+                "bookName": "over the lazy dogThe qui",
+                "author": "Roger",
+                "price": 5683.48,
+                "createdAt": "2015-07-20T21:22:45.900Z",
+                "updatedAt": "2012-10-25T07:32:03.969Z"
+            },
+            {
+
+                "bookName": "quick brown fox j",
+                "author": "Kyle",
+                "price": 3503.9,
+                "createdAt": "1998-11-11T01:51:10.298Z",
+                "updatedAt": "2012-08-24T21:41:47.052Z"
+            },
+            {
+
+                "bookName": "fox jumps over the la",
+                "author": "Chad",
+                "price": 6161.81,
+                "createdAt": "2000-04-11T11:45:29.272Z",
+                "updatedAt": "2008-09-09T02:26:00.788Z"
+            },
+            {
+
+                "bookName": "quick brown fox j",
+                "author": "John",
+                "price": 9812.58,
+                "createdAt": "1997-10-14T21:50:58.297Z",
+                "updatedAt": "1982-11-05T19:36:00.859Z"
+            },
+            {
+
+                "bookName": "gThe quick",
+                "author": "Adel",
+                "price": 3011.81,
+                "createdAt": "2006-07-18T21:55:12.887Z",
+                "updatedAt": "1989-07-13T03:18:56.307Z"
+            },
+            {
+
+                "bookName": "jumps over the lazy dog",
+                "author": "Glenda",
+                "price": 4700.73,
+                "createdAt": "1993-06-01T22:05:14.494Z",
+                "updatedAt": "1985-08-25T05:00:59.247Z"
+            },
+            {
+
+                "bookName": "over the lazy dogThe quick brown",
+                "author": "Hilda",
+                "price": 9730.3,
+                "createdAt": "2013-03-28T14:09:51.599Z",
+                "updatedAt": "1982-04-22T18:20:50.028Z"
+            },
+            {
+
+                "bookName": "fox jumps over the lazy dogThe quic",
+                "author": "Jenna",
+                "price": 2941.24,
+                "createdAt": "2015-05-27T21:21:03.548Z",
+                "updatedAt": "1980-06-21T10:39:16.693Z"
+            },
+            {
+
+                "bookName": "dogThe quick brown fox jumps over the l",
+                "author": "Irene",
+                "price": 2772.21,
+                "createdAt": "1980-04-20T14:56:23.996Z",
+                "updatedAt": "2015-10-27T09:01:15.634Z"
+            }
+        ], {});
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        return queryInterface.bulkDelete('Books', null, {});
+    }
+};
+
+
+
