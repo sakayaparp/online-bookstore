dd10b14922642a8530daa06c7073343206c7d261
Kanin Kearpimy <kaninkearpimy@Kanins-MacBook-Pro.local>
2021-12-15 11:52:18 +0700

    [Game & James] - usecase for create-book

--- packages/inventory/domain/use-cases/create-book/create-book.test.ts
+++ packages/inventory/domain/use-cases/create-book/create-book.test.ts
@@ -1,4 +1,4 @@
-import { CreateBook, CreateBookDTO, BookRepoInterface } from './create-book'
+import { CreateBook, CreateBookDTO, BookRepoInterface } from "./create-book";
 import { Amount } from "../../value-object/amount";
 import { ISBN } from "../../value-object/isbn";
 import { Money } from "../../value-object/money";
@@ -6,21 +6,47 @@ import { Name } from "../../value-object/name";
 import { UniqueEntityID } from "../../../../share/domain/unique-entity-id";
 
 describe("use case create book test", () => {
-    test("create book", () => {
-        let bookRepo: BookRepoInterface;
-        let book: CreateBookDTO = {
-            ISBN: ISBN.create("9971-5-0210-0"),
-            title: "Mock Book Name",
-            authors: [Name.create("Mock Name")],
-            amount: Amount.create(500),
-            price: Money.create({ value: 100, currency: "BTC" }),
-            coverImage: "./image.jpg",
-            description: "Lopem Lopem Lopem",
-            categoryId: new UniqueEntityID("cate-1"),
-        }
-        const createBook = new CreateBook(bookRepo);
+  test("create book", () => {
+    let bookRepo: BookRepoInterface = {
+      save: jest.fn(),
+      hasBookById: jest.fn(() => false),
+    };
+    let book: CreateBookDTO = {
+      ISBN: ISBN.create("9971-5-0210-0"),
+      title: "Mock Book Name",
+      authors: [Name.create("Mock Name")],
+      amount: Amount.create(500),
+      price: Money.create({ value: 100, currency: "BTC" }),
+      coverImage: "./image.jpg",
+      description: "Lopem Lopem Lopem",
+      categoryId: new UniqueEntityID("cate-1"),
+    };
+    const createBook = new CreateBook(bookRepo);
 
+    createBook.execute(book);
+    expect(bookRepo.save).toHaveBeenCalled();
+  });
 
-        createBook.execute(book);
-    })
-})
+  test("create book duplicate", () => {
+    let bookRepo: BookRepoInterface = {
+      save: jest.fn(),
+      hasBookById: jest.fn(() => true),
+    };
+    let book: CreateBookDTO = {
+      ISBN: ISBN.create("9971-5-0210-0"),
+      title: "Mock Book Name",
+      authors: [Name.create("Mock Name")],
+      amount: Amount.create(500),
+      price: Money.create({ value: 100, currency: "BTC" }),
+      coverImage: "./image.jpg",
+      description: "Lopem Lopem Lopem",
+      categoryId: new UniqueEntityID("cate-1"),
+    };
+    const createBook = new CreateBook(bookRepo);
+    const throwErrorMock = async () => {
+      console.warn(await createBook.execute(book));
+    };
+
+    expect(throwErrorMock).toThrowError("Duplicate ISBN key");
+  });
+});
--- packages/inventory/domain/use-cases/create-book/create-book.ts
+++ packages/inventory/domain/use-cases/create-book/create-book.ts
@@ -1,35 +1,38 @@
-import { ISBN } from '../../value-object/isbn'
-import { Name } from '../../value-object/name'
-import { Amount } from '../../value-object/amount'
-import { Money } from '../../value-object/money'
-import { UniqueEntityID } from '../../../../share/domain/unique-entity-id'
-import { Book } from '../../book'
+import { ISBN } from "../../value-object/isbn";
+import { Name } from "../../value-object/name";
+import { Amount } from "../../value-object/amount";
+import { Money } from "../../value-object/money";
+import { UniqueEntityID } from "../../../../share/domain/unique-entity-id";
+import { Book } from "../../book";
 
 export interface BookRepoInterface {
-    save: (data: any) => Promise<any>;
+  save: (data: any) => Promise<any>;
+  hasBookById: (id: ISBN) => boolean;
 }
 
 export interface CreateBookDTO {
-    ISBN: ISBN;
-    title: string;
-    authors: Name[];
-    amount: Amount;
-    price: Money;
-    coverImage: string;
-    description: string;
-    categoryId: UniqueEntityID;
+  ISBN: ISBN;
+  title: string;
+  authors: Name[];
+  amount: Amount;
+  price: Money;
+  coverImage: string;
+  description: string;
+  categoryId: UniqueEntityID;
 }
 
 export class CreateBook {
-    private bookRepo: BookRepoInterface;
+  private bookRepo: BookRepoInterface;
 
-    constructor(bookRepo: BookRepoInterface) {
-        this.bookRepo = bookRepo;
-    }
+  constructor(bookRepo: BookRepoInterface) {
+    this.bookRepo = bookRepo;
+  }
 
-    public async execute(request: CreateBookDTO) {
-        // TODO: check dup book
-        const book = Book.create(request)
-        await this.bookRepo.save(book)
+  public async execute(request: CreateBookDTO) {
+    if (this.bookRepo.hasBookById(request.ISBN)) {
+      throw new Error("Duplicate ISBN key");
     }
+    const book = Book.create(request);
+    await this.bookRepo.save(book);
+  }
 }
