8c983b41c0a3dd84fd7040873f2e3ce902841b21
pwarintorn <warintorn.phusomsai@thoughtworks.com>
2021-12-15 10:22:46 +0700

    add increase item test

--- packages/bookstore/cart/items/item.test.ts
+++ packages/bookstore/cart/items/item.test.ts
@@ -2,8 +2,20 @@ import {Item} from './item'
 import {UniqueEntityID} from "../../../share/domain/unique-entity-id";
 
 describe('item', () => {
-    const item = Item.create("cat", 2, 5);
     test('calculate total price', () => {
+      const item = Item.create("cat", 2, 5);
         expect(item.totalPrice()).toBe(10);
     });
+
+    test('increase item amount', () => {
+      const item = Item.create("cat", 2, 5);
+      expect(item.increaseAmount().amount).toBe(3)
+  });
+  test('increase 2 item `amount', () => {
+    const item = Item.create("cat", 2, 5);
+    const increasedItem1 = item.increaseAmount()
+    expect(increasedItem1.amount).toBe(3)
+    const increasedItem2 = increasedItem1.increaseAmount()
+    expect(increasedItem2.amount).toBe(4)
+});
 });
