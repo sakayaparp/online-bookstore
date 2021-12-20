bd9e8c0e377c78a3f1bd643796f7fbf1604596e2
Kanin Kearpimy <kaninkearpimy@Kanins-MacBook-Pro.local>
2021-12-20 14:50:12 +0700

    [Em & Game & James] - Cart db migration

---
+++ packages/cart/__db__/TestConnection.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize";
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize("postgres://admin:admin@localhost:5432/cart"); // Example for postgres
+console.log("before async");
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully");
+} catch (error) {
+  console.log("Unable to connection to db", error);
+}
+
+console.log("after async");
