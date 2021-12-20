1a534fa3ee611f4ba1f6c69d54be8458c68179a8
Kanin Kearpimy <kaninkearpimy@Kanins-MacBook-Pro.local>
2021-12-20 15:00:26 +0700

    [Em & Game & James] - Pull out inventory

--- packages/cart/subscriptions/afterBookCreated.ts
+++ packages/cart/subscriptions/afterBookCreated.ts
@@ -1,29 +1,30 @@
-import {Poller} from "../../inventory/infra/poller"
-import {CreateItemUseCase} from "../useCases/createItem/CreateItemUseCase"
-import {CreateItemDTO} from "../useCases/createItem/CreateItemDTO"
+import { Poller } from "../../inventory/infra/poller";
+import { CreateItemUseCase } from "../useCases/createItem/CreateItemUseCase";
+import { CreateItemDTO } from "../useCases/createItem/CreateItemDTO";
 
 export class AfterBookCreated {
-    private createItem: CreateItemUseCase;
+  private createItem: CreateItemUseCase;
 
-    constructor(usecase: CreateItemUseCase) { // TODO: use CreateBookUseCase instead
-        this.setUpSupsctiptions()
-        this.createItem = usecase
-    }
+  constructor(usecase: CreateItemUseCase) {
+    // TODO: use CreateBookUseCase instead
+    this.setUpSupsctiptions();
+    this.createItem = usecase;
+  }
 
-    public setUpSupsctiptions() {
-        Poller.create().book.on("book:created", async (data: any) => {
-            console.log("[SUBSCRIBE AfterBookCreated]", data)
-            await this.onItemCreated(data)
-        })
-    }
+  public setUpSupsctiptions() {
+    Poller.create().book.on("book:created", async (data: any) => {
+      console.log("[SUBSCRIBE AfterBookCreated]", data);
+      await this.onItemCreated(data);
+    });
+  }
 
-    async onItemCreated(event: any): Promise<void> {
-        const detail: CreateItemDTO = {
-            name: event.payload.props.title,
-            amount: event.payload.props.amount,
-            price: event.payload.props.price
-        };
+  async onItemCreated(event: any): Promise<void> {
+    const detail: CreateItemDTO = {
+      name: event.payload.props.title,
+      amount: event.payload.props.amount,
+      price: event.payload.props.price,
+    };
 
-        await this.createItem.execute(detail, event.payload._id);
-    }
+    await this.createItem.execute(detail, event.payload._id);
+  }
 }
--- packages/inventory/index.ts
+++
@@ -1,2 +0,0 @@
-// infra
-import "./infra/app"
