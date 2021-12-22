df4338537a6800cb8fada1c02aedbeb49eb15cda
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-22 13:48:54 +0700

    Cleaned up project.

--- packages/inventory/domain/repos/bookRepo.ts
+++
@@ -1,8 +0,0 @@
-import { ISBN } from "../value-object/isbn";
-import { Book } from "../book"
-
-export interface IBookRepoInterface {
-    findAll: () => Promise<Book[]>;
-    save: (data: Book) => Promise<Book>;
-    hasBookById: (id: ISBN) => boolean;
-}
