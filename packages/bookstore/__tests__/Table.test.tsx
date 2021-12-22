09337a44961a4ba655456614bc4985db7227293b
Kanin Kearpimy <kaninkearpimy@Kanins-MacBook-Pro.local>
2021-12-22 13:55:04 +0700

    Add test to TableCustome Component

--- packages/bookstore/__tests__/Table.test.tsx
+++ packages/bookstore/__tests__/Table.test.tsx
@@ -5,7 +5,11 @@ describe("test Table component", () => {
   it("correctly rendered", () => {
     const books = [{ ISBN: "9971-5-0210-0", amount: 500 }];
     render(<TableCustome thead={books[0]} tbody={books} />);
+    const tbody = screen.getByTestId("table-component-tbody");
 
     expect(screen.getByText("ISBN")).toBeInTheDocument();
+    expect(screen.getByText("amount")).toBeInTheDocument();
+    expect(tbody.querySelectorAll("tr").length).toBe(1);
+    expect(tbody.querySelector("tr td").innerHTML).toEqual("9971-5-0210-0");
   });
 });
