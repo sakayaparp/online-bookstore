237dfb80579919655e425029f11928d965d59ab4
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-22 15:09:27 +0700

    Modified save repo.

--- packages/inventory-api/src/domain/repos/implementations/sequelizeBookRepo.ts
+++ packages/inventory-api/src/domain/repos/implementations/sequelizeBookRepo.ts
@@ -1,34 +1,37 @@
-import { IBookRepoInterface } from "../bookRepo";
-import { ISBN } from "../../value-object/isbn";
-import { Book } from "../../book";
-import { eventRepo } from "../../repos";
-import { Sequelize } from "sequelize/dist";
-import { QueryTypes } from "sequelize";
+import {IBookRepoInterface} from "../bookRepo";
+import {ISBN} from "../../value-object/isbn";
+import {Book} from "../../book";
+import {Sequelize} from "sequelize/dist";
 
 export class SequelizeBookRepo implements IBookRepoInterface {
-  public books: Book[] = [];
-  public sequelize: Sequelize;
-  constructor(sequalize: Sequelize) {
-    this.sequelize = sequalize;
-  }
+    public books: Book[] = [];
+    public sequelize: Sequelize;
 
-  async findAll(): Promise<any> {
-    return await this.sequelize.query("SELECT * FROM inventories", {
-      type: QueryTypes.SELECT,
-    });
-  }
+    constructor(sequalize: Sequelize) {
+        this.sequelize = sequalize;
+    }
 
-  async save(data: Book): Promise<Book> {
-    // this.books.push(data);
-    // data.domainEvents.forEach((v) => {
-    //   eventRepo.save(v);
-    // });
-    // return data;
-    return data;
-  }
+    async findAll(): Promise<any> {
+        return await this.sequelize.model("inventories").findAll();
+    }
 
-  hasBookById(id: ISBN): boolean {
-    let result = this.books.filter((v) => v.isbn === id);
-    return result.length >= 1;
-  }
+    async save(data: Book): Promise<Book> {
+        this.sequelize.model("inventories").create(data.props)
+            .then(value => {
+                // save event
+                console.log('Save book successfully')
+            })
+        // this.books.push(data);
+        // data.domainEvents.forEach((v) => {
+        //   eventRepo.save(v);
+        // });
+        // return data;
+
+        return data;
+    }
+
+    hasBookById(id: ISBN): boolean {
+        let result = this.books.filter((v) => v.isbn === id);
+        return result.length >= 1;
+    }
 }
--- packages/inventory-api/src/domain/repos/index.ts
+++ packages/inventory-api/src/domain/repos/index.ts
@@ -1,20 +1,10 @@
 import { SequelizeBookRepo } from "./implementations/sequelizeBookRepo";
 import { SequelizeEventRepo } from "./implementations/sequelizeEventRepo";
+import models from '../../../models';
 
-import { Sequelize } from "sequelize";
 
-// Passing a connection URI
-const sequelize = new Sequelize(
-  "postgres://admin:admin@localhost:5432/inventory"
-);
 
-try {
-  sequelize.authenticate();
-} catch (error) {
-  console.log("Unable to connection to db", error);
-}
-
-const bookRepo = new SequelizeBookRepo(sequelize);
+const bookRepo = new SequelizeBookRepo(models.sequelize);
 const eventRepo = new SequelizeEventRepo();
 
 export { bookRepo, eventRepo };
